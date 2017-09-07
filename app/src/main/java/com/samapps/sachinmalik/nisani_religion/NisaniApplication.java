ec2a6e4a702520d0165a85d54611907268587a66
Sachin Malik <sachin.15085@ggnindia.dronacharya.info>
2017-09-07 17:01:45 +0530

    new changes

--- app/src/main/java/com/samapps/sachinmalik/nisani_religion/NisaniApplication.java
+++ app/src/main/java/com/samapps/sachinmalik/nisani_religion/NisaniApplication.java
@@ -1,10 +1,39 @@
 package com.samapps.sachinmalik.nisani_religion;
 
 import android.app.Application;
+import android.content.res.Configuration;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by Sachin Malik on 07-Sep-17.
  */
 
 public class NisaniApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Fabric.with(this, new Crashlytics());
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+
+    @Override
+    public void onTrimMemory(int level) {
+        super.onTrimMemory(level);
+    }
 }
