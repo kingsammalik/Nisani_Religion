40004489736b57f368c6f27f0e8cb09279bc43d9
Sachin Malik <sachin.15085@ggnindia.dronacharya.info>
2017-09-07 12:45:21 +0530

    new changes

---
+++ app/src/main/java/com/samapps/sachinmalik/nisani_religion/NisaniApplication.java
@@ -0,0 +1,10 @@
+package com.samapps.sachinmalik.nisani_religion;
+
+import android.app.Application;
+
+/**
+ * Created by Sachin Malik on 07-Sep-17.
+ */
+
+public class NisaniApplication extends Application {
+}
